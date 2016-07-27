6cf8cb05b72628b00d5cce13e0d0ecc188e6d754
Bhanu <bhanusruti512@gmail.com>
2016-07-26 19:06:43 -0700

    ecommerce-app coloring and fonts

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,51 @@
-class ProductsController < ApplicationController
+  class ProductsController < ApplicationController
 
-  def index
-    @products = Product.all
+    def index
+      @products = Product.all 
+    end
+
+    def show
+      # get specific  product
+      @product = Product.find_by(id: params[:id])
+    end
+
+    def new
+
+    end
+
+    def create
+      name = params[:name]
+      price = params[:price]
+      breed= params[:breed]
+      color= params[:color]
+       product = Product.new(name: name, price: price, breed: breed, color:color)
+       product.save
+       flash[:success] = "Product Created!!!!!"
+       redirect_to "/products/#{product.id}"
+    end
+
+    def edit
+      @id = params[:id]
+      @product = Product.find_by(id: params[:id])
+    end
+
+    def update
+      name = params[:name]
+      price = params[:price]
+      breed= params[:breed]
+      color= params[:color]
+      @product = Product.find_by(id: params[:id])
+      @product.update(name: name, price:price, color: color, breed: breed)
+      product.save
+      flash[:success] = "Product Updated"
+      redirect_to "/products/#{product.id}"
+    end
+
+    def destroy
+      product = Product.find_by(id: params[:id])
+      product.delete
+      flash[:warning] = "Product Deleted"
+      redirect_to "/products"
+    end
+
   end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'products#index'
+  get '/products', to:'products#index'
+
+  get '/products/new', to: 'products#new' 
+  get '/products/:id', to: 'products#show'
+
+  # we r makinng a new product
+  post '/products', to: 'products#create'
+
+  get '/products/:id/edit', to: 'products#edit'
+  put '/products/:id', to: 'products#update'
+
+  delete 'products/:id',to: 'products#destroy'
 end
